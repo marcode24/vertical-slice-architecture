f5f89f5ae185cb80a6807be647cb477f0aaca3cb
Marco Cruz <marco24cruz08@gmail.com>
2024-10-24 11:04:36 -0600

    ♻️ Refactor resilience pipeline in CatalogoApiClient

--- ShoppingCart/ShoppingCart.Api/Program.cs
+++ ShoppingCart/ShoppingCart.Api/Program.cs
@@ -1,4 +1,7 @@
+using Polly;
+using Polly.Fallback;
 using ShoppingCart.Api.Features.Catalogo;
+using ShoppingCart.Api.Shared.Domain.Models;
 using ShoppingCart.Api.Shared.Networking.CatalogoApi;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,6 +18,19 @@ builder.Services.AddHttpClient<CatalogoApiService>((serviceProvider, httpClient)
     PooledConnectionLifetime = TimeSpan.FromMinutes(5)
   };
 }).SetHandlerLifetime(Timeout.InfiniteTimeSpan);
+
+
+builder.Services
+  .AddResiliencePipeline<string, IEnumerable<Catalogo>>(
+    "catalogo-products",
+    pipelineBuilder =>
+    {
+      pipelineBuilder.AddFallback(new FallbackStrategyOptions<IEnumerable<Catalogo>>
+      {
+        FallbackAction = _ => Outcome
+          .FromResultAsValueTask<IEnumerable<Catalogo>>(Enumerable.Empty<Catalogo>()),
+      });
+    });
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
--- ShoppingCart/ShoppingCart.Api/Shared/Networking/CatalogoApi/CatalogoApiClient.cs
+++ ShoppingCart/ShoppingCart.Api/Shared/Networking/CatalogoApi/CatalogoApiClient.cs
@@ -1,16 +1,19 @@
 using Polly;
+using Polly.Registry;
 using ShoppingCart.Api.Shared.Domain.Models;
 
 namespace ShoppingCart.Api.Shared.Networking.CatalogoApi;
 
 public sealed class CatalogoApiClient(
   CatalogoApiService catalogoApiService,
-  ILoggerFactory loggerFactory)
+  ILoggerFactory loggerFactory,
+  ResiliencePipelineProvider<string> pipelineProvider)
   : ICatalogoApiClient
 {
 
   private readonly CatalogoApiService _catalogoApiService = catalogoApiService;
   private readonly ILoggerFactory _loggerFactory = loggerFactory;
+  private readonly ResiliencePipelineProvider<string> _pipelineProvider = pipelineProvider;
 
   public Task<Catalogo> GetProductByCodeAsync(string code, CancellationToken cancellationToken)
   {
@@ -20,18 +23,22 @@ public sealed class CatalogoApiClient(
   public async Task<IEnumerable<Catalogo>> GetProductsAsync(CancellationToken cancellationToken)
   {
     var logger = _loggerFactory.CreateLogger("RetryLog");
-    var policy = Policy.Handle<ApplicationException>()
-      .WaitAndRetryAsync(
-        3,
-        retryAttempt =>
-        {
-          logger.LogInformation($"Retry count: {retryAttempt}");
-          var timeToRetry = TimeSpan.FromSeconds(Math.Pow(2, retryAttempt));
-          return timeToRetry;
-        });
+    // var policy = Policy.Handle<ApplicationException>()
+    //   .WaitAndRetryAsync(
+    //     3,
+    //     retryAttempt =>
+    //     {
+    //       logger.LogInformation($"Retry count: {retryAttempt}");
+    //       var timeToRetry = TimeSpan.FromSeconds(Math.Pow(2, retryAttempt));
+    //       return timeToRetry;
+    //     });
 
-    var products = await policy.ExecuteAsync(()
-      => _catalogoApiService.GetProductsAsync(cancellationToken));
+    // var products = await policy.ExecuteAsync(()
+    //   => _catalogoApiService.GetProductsAsync(cancellationToken));
+
+    var pipeline = _pipelineProvider.GetPipeline<IEnumerable<Catalogo>>("catalogo-products");
+    var products = await pipeline
+      .ExecuteAsync(async token => await _catalogoApiService.GetProductsAsync(token), cancellationToken);
 
     return products;
   }
